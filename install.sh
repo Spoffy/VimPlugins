29b4f53a24c98b6f72c51a02ffdf6ed4fc4912b4
Spoffy <spoffeh@gmail.com>
2014-03-25 15:53:49 +0000

    Working install script

--- install.sh
+++ install.sh
@@ -3,14 +3,12 @@
 echo -e "\e[31mCloning Vundle\e[0m" &&
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 
+if [ ! -h ~/.vimrc ]; then
+    echo -e "\e[31mSymlinking ~/.vimrc to ~/.vim/vimrc\e[0m" &&
+    ln -s ~/.vim/vimrc ~/.vimrc
+fi
+
 echo -e "\e[31mInstalling Bundles\e[0m" &&
 vim +PluginInstall +qall &&
 echo -e "\e[31mBundles installation complete (probably)!\e[0m"
 
-if [ ! -h ~/.vimrc ]; then
-    ln -s ~/.vim/vimrc ~/.vimrc
-fi
-
-if [ -a ~/.vim/pre_install_vimrc ]; then
-    mv ~/.vim/pre_install_vimrc ~/.vim/vimrc
-fi
