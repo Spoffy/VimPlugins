c44b9f876285b368194274356bf5b8ffc296e661
Spoffy <spoffeh@gmail.com>
2015-09-11 15:38:02 +0100

    Fixed an infinite loop bug in install.sh caused by removing YouCompleteMe

--- install.sh
+++ install.sh
@@ -11,8 +11,3 @@ fi
 echo -e "\e[31mInstalling Bundles\e[0m" &&
 vim +PluginInstall +qall &&
 echo -e "\e[31mBundles installation complete (probably)!\e[0m"
-
-echo -e "\e[31mBuilding YouCompleteMe[\0m" &&
-cd bundle/YouCompleteme
-./install.sh
-cd ../..
