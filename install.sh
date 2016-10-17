607c759d2ec55dd7bf69ef4a41d3d576fb05bcc6
Spoffy <spoffeh@gmail.com>
2016-10-17 18:07:47 +0100

    Improved install.sh to copy vimrc or emit a warning if vimrc already exists.

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 echo -e "\e[31mCloning Vundle\e[0m" &&
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle;
 
 if [ ! -h ~/.vimrc ]; then
+    echo -e "\e[31mCopying vimrc to ~/.vim/vimrc\e[0m" &&
+    cp vimrc ~/.vim/vimrc;
+
     echo -e "\e[31mSymlinking ~/.vimrc to ~/.vim/vimrc\e[0m" &&
     ln -s ~/.vim/vimrc ~/.vimrc
+else
+    echo -e "\e[31mvimrc already exists, doing nothing\e[0m"
 fi
 
 echo -e "\e[31mInstalling Bundles\e[0m" &&
